a0004be6eeacf06671aadacc53b51ea5c68a3827
Filiperibas <fellps77.plo@gmail.com>
2025-03-31 15:34:48 -0400

    correções

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cardapio-bolt.new/',
   plugins: [react()],
-  base: "/cardapio-bolt.new",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
